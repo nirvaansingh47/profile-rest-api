05c661c8d4a91ac6b9b59724732f4227c431b284
NirvaanSingh <nirvaan.singh@vumatel.co.za>
2020-01-28 21:50:37 +0200

    Added feed api with permissions

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -29,4 +29,14 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+
+class UserProfileFeedSerializer(serializers.ModelSerializer):
+    """Serializer for a profile feed item"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only':True }}
+        
     
